9d74483fbc67861d81ab05c867b64be2c29770ba
Mr9J <91mr.ya@gmail.com>
2024-03-05 16:29:48 +0800

    add dash

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,12 +8,22 @@ public class Player : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
+    [Header("Dash Info")]
+    public float dashSpeed;
+    public float dashDuration;
+    public float dashCooldown;
+    public float dashDir { get; private set; }
+    private float dashUsageTimer;
+
     [Header("Collision Info")]
     [SerializeField] private Transform grounCheck;
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private Transform wallCheck;
     [SerializeField] private float wallCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
+
+    public int facingDir { get; private set; } = 1;
+    private bool facingRight = true;
 
     #region Components
     public Animator anim { get; private set; }
@@ -26,6 +36,7 @@ public class Player : MonoBehaviour
     public PlayerMoveState moveState { get; private set; }
     public PlayerJumpState jumpState { get; private set; }
     public PlayerAirState airState { get; private set; }
+    public PlayerDashState dashState { get; private set; }
     #endregion
 
     private void Awake()
@@ -36,6 +47,7 @@ public class Player : MonoBehaviour
         moveState = new PlayerMoveState(this, stateMachine, "Move");
         jumpState = new PlayerJumpState(this, stateMachine, "Jump");
         airState = new PlayerAirState(this, stateMachine, "Jump");
+        dashState = new PlayerDashState(this, stateMachine, "Dash");
     }
 
     private void Start()
@@ -49,11 +61,13 @@ public class Player : MonoBehaviour
     private void Update()
     {
         stateMachine.currentState.Update();
+        CheckForDashInput();
     }
 
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
     }
 
     public bool IsGroundDetected() => Physics2D.Raycast(grounCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
@@ -63,5 +77,43 @@ public class Player : MonoBehaviour
     {
         Gizmos.DrawLine(grounCheck.position, new Vector3(grounCheck.position.x, grounCheck.position.y - groundCheckDistance));
         Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+    private void Flip()
+    {
+        facingDir *= -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController(float _x)
+    {
+        if (_x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        if (_x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void CheckForDashInput()
+    {
+        dashUsageTimer -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashUsageTimer < 0)
+        {
+            dashUsageTimer = dashCooldown;
+
+            dashDir = Input.GetAxisRaw("Horizontal");
+
+            if (dashDir == 0)
+            {
+                dashDir = facingDir;
+            }
+
+            stateMachine.ChangeState(dashState);
+        }
     }
 }
