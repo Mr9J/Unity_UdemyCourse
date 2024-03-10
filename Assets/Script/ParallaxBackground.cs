bc07a155166fe3aba070652ef41667a65990ea50
ChengYe Sie <91mr.ya@gmail.com>
2024-03-10 11:01:41 +0800

    Parallax e

--- Assets/Script/ParallaxBackground.cs
+++ Assets/Script/ParallaxBackground.cs
@@ -10,10 +10,16 @@ public class ParallaxBackground : MonoBehaviour
 
     private float xPosition;
     private float yPosition;
+    private float length;
+    private float height;
+
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.Find("Main Camera");
+
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
 
         xPosition = transform.position.x;
         yPosition = transform.position.y;
@@ -22,9 +28,29 @@ public class ParallaxBackground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float distanceMovedX = cam.transform.position.x * (1 - parallaxEffect);
+        float distanceMovedY = cam.transform.position.y * (1 - parallaxEffect);
         float distanceToMoveX = cam.transform.position.x * parallaxEffect;
         float distnaceToMoveY = cam.transform.position.y * parallaxEffect;
 
         transform.position = new Vector3(xPosition + distanceToMoveX, yPosition + distnaceToMoveY);
+
+        if (distanceMovedX > xPosition + length)
+        {
+            xPosition = xPosition + length;
+        }
+        else if (distanceMovedX < xPosition - length)
+        {
+            xPosition = xPosition - length;
+        }
+
+        if (distanceMovedY > yPosition + height)
+        {
+            yPosition = yPosition + height;
+        }
+        else if (distanceMovedY < yPosition - height)
+        {
+            yPosition = yPosition - height;
+        }
     }
 }
