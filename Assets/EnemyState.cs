573df61a12efe07bbbad80b7763e11da00d1fe2e
ChengYe Sie <91mr.ya@gmail.com>
2024-03-11 22:35:25 +0800

    create entity

---
+++ Assets/EnemyState.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState
+{
+    protected EnemyStateMachine stateMachine;
+    protected Enemy enemy;
+
+    private string animBoolName;
+
+    protected float stateTimer;
+    protected bool triggerCalled;
+
+    public EnemyState(Enemy _enemy, EnemyStateMachine _stateMachine, string _animBoolName)
+    {
+        this.enemy = _enemy;
+        this.stateMachine = _stateMachine;
+        this.animBoolName = _animBoolName;
+    }
+
+    public virtual void Enter()
+    {
+        triggerCalled = true;
+        enemy.anim.SetBool(animBoolName, true);
+    }
+
+    public virtual void Exit()
+    {
+        enemy.anim.SetBool(animBoolName, false);
+    }
+
+    public virtual void Update()
+    {
+        stateTimer -= Time.deltaTime;
+    }
+}
--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Entity
 {
     public bool isBusy { get; private set; }
 
@@ -20,20 +20,11 @@ public class Player : MonoBehaviour
     [Header("Attack Details")]
     public Vector2[] attackMovement;
 
-    [Header("Collision Info")]
-    [SerializeField] private Transform grounCheck;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private Transform wallCheck;
-    [SerializeField] private float wallCheckDistance;
-    [SerializeField] private LayerMask whatIsGround;
 
-    public int facingDir { get; private set; } = 1;
-    private bool facingRight = true;
 
-    #region Components
-    public Animator anim { get; private set; }
-    public Rigidbody2D rb { get; private set; }
-    #endregion
+
+
+
 
     #region States
     public PlayerStateMachine stateMachine { get; private set; }
@@ -47,8 +38,10 @@ public class Player : MonoBehaviour
     public PlayerPrimaryAttackState primaryAttack { get; private set; }
     #endregion
 
-    private void Awake()
+    protected override void Awake()
     {
+        base.Awake();
+
         stateMachine = new PlayerStateMachine();
 
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
@@ -62,16 +55,17 @@ public class Player : MonoBehaviour
         primaryAttack = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
     }
 
-    private void Start()
+    protected override void Start()
     {
-        anim = GetComponentInChildren<Animator>();
-        rb = GetComponent<Rigidbody2D>();
+        base.Start();
 
         stateMachine.Initialize(idleState);
     }
 
-    private void Update()
+    protected override void Update()
     {
+        base.Update();
+
         stateMachine.currentState.Update();
 
         CheckForDashInput();
@@ -85,40 +79,6 @@ public class Player : MonoBehaviour
     }
 
     public void AnimationTrigger() => stateMachine.currentState.AnimationFinishTrigger();
-
-    public void SetVelocity(float _xVelocity, float _yVelocity)
-    {
-        rb.velocity = new Vector2(_xVelocity, _yVelocity);
-        FlipController(_xVelocity);
-    }
-
-    public bool IsGroundDetected() => Physics2D.Raycast(grounCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
-    public bool IsWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(grounCheck.position, new Vector3(grounCheck.position.x, grounCheck.position.y - groundCheckDistance));
-        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
-    }
-
-    private void Flip()
-    {
-        facingDir *= -1;
-        facingRight = !facingRight;
-        transform.Rotate(0, 180, 0);
-    }
-
-    private void FlipController(float _x)
-    {
-        if (_x > 0 && !facingRight)
-        {
-            Flip();
-        }
-        if (_x < 0 && facingRight)
-        {
-            Flip();
-        }
-    }
 
     private void CheckForDashInput()
     {
